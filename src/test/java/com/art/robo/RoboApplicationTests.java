3c2291de5f6f67a797e9f551b6675a87aa041f1d
mihran <mihran.eprem@gmail.com>
2022-01-04 22:15:40 -0300

    start

---
+++ src/test/java/com/art/robo/RoboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.art.robo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
